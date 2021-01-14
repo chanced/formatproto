58cd10f3a4c8603e008399e9ddd403588581cd68
Chance <chanceusc@gmail.com>
2021-01-14 16:37:40 -0500

    Create formatproto.go

---
+++ formatproto.go
@@ -0,0 +1,59 @@
+package formatproto
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"path"
+	"strings"
+
+	pgs "github.com/lyft/protoc-gen-star"
+)
+
+type formatProto struct {
+	currentFile string
+}
+
+// FormatProto formats proto files with clang-format
+func FormatProto() pgs.PostProcessor {
+	return &formatProto{}
+}
+
+func (p *formatProto) Match(a pgs.Artifact) bool {
+	var n string
+	switch a := a.(type) {
+	case pgs.GeneratorFile:
+		n = a.Name
+	case pgs.GeneratorTemplateFile:
+		n = a.Name
+	case pgs.CustomFile:
+		n = a.Name
+	case pgs.CustomTemplateFile:
+		n = a.Name
+	default:
+		return false
+	}
+	p.currentFile = n
+	return strings.HasSuffix(n, ".proto")
+
+}
+
+func (p formatProto) Process(b []byte) ([]byte, error) {
+
+	in := strings.NewReader(string(b))
+	out := &bytes.Buffer{}
+	errbuf := &bytes.Buffer{}
+	filename := path.Base(p.currentFile)
+
+	cmd := exec.Command("clang-format", fmt.Sprintf("--assume-filename=%s", filename))
+	cmd.Stdin = in
+	cmd.Stdout = out
+	cmd.Stderr = errbuf
+	if err := cmd.Run(); err != nil {
+		panic(fmt.Sprintf("%v\n %s", err, errbuf))
+	}
+
+	return out.Bytes(), nil
+}
+
+var _ pgs.PostProcessor = &formatProto{}
